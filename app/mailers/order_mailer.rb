54a062b88f8bc39855c484ef357c9ba2d39ce9df
nguyenvanson332000 <nguyenvanson332000@gmail.com>
2022-05-13 09:47:12 +0700

    update status order by admin

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,8 @@
+class OrderMailer < ApplicationMailer
+  def order_status order, order_detail, total
+    @order = order
+    @order_detail = order_detail
+    @total = total
+    mail to: @order.user_email, subject: t("order_mailer.title")
+  end
+end
